461a0f5256031171c421438f993f48d48593af03
Jeremy Kutil <jeremykutil@gmail.com>
2019-02-06 20:48:49 -0600

    added some test data helper methods
    working on deal test

--- src/test/java/CardsTest.java
+++ src/test/java/CardsTest.java
@@ -1,10 +1,13 @@
 import io.kutilj.cards.Card;
 import io.kutilj.cards.Dealer;
 import io.kutilj.cards.Deck;
-import org.testng.annotations.BeforeTest;
+import io.kutilj.cards.Player;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
@@ -19,7 +22,7 @@ public class CardsTest {
     /**
      * Test setup method that initializes the deck and the dealer
      */
-    @BeforeTest()
+    @BeforeMethod()
     public void setup() {
 
         dealer = new Dealer(createFullDeck());
@@ -74,9 +77,13 @@ public class CardsTest {
     @Test
     public void testDeal() {
 
-        dealer.deal(8);
+        List<Player> players = createPlayers(10);
+
+        dealer.shuffle();
+        dealer.deal(players, 5);
 
         // validate all players received cards
+        players.forEach(System.out::println);
 
     }
 
@@ -89,62 +96,68 @@ public class CardsTest {
 
         return new Deck()
                 .withCards(
-                        Arrays.asList(
-                                new Card("Ace", "Clubs", 1),
-                                new Card("2", "Clubs", 2),
-                                new Card("3", "Clubs", 3),
-                                new Card("4", "Clubs", 4),
-                                new Card("5", "Clubs", 5),
-                                new Card("6", "Clubs", 6),
-                                new Card("7", "Clubs", 7),
-                                new Card("8", "Clubs", 8),
-                                new Card("9", "Clubs", 9),
-                                new Card("10", "Clubs", 10),
-                                new Card("Jack", "Clubs", 11),
-                                new Card("Queen", "Clubs", 12),
-                                new Card("King", "Clubs", 13),
-                                new Card("Ace", "Hearts", 1),
-                                new Card("2", "Hearts", 2),
-                                new Card("3", "Hearts", 3),
-                                new Card("4", "Hearts", 4),
-                                new Card("5", "Hearts", 5),
-                                new Card("6", "Hearts", 6),
-                                new Card("7", "Hearts", 7),
-                                new Card("8", "Hearts", 8),
-                                new Card("9", "Hearts", 9),
-                                new Card("10", "Hearts", 10),
-                                new Card("Jack", "Hearts", 11),
-                                new Card("Queen", "Hearts", 12),
-                                new Card("King", "Hearts", 13),
-                                new Card("Ace", "Spades", 1),
-                                new Card("2", "Spades", 2),
-                                new Card("3", "Spades", 3),
-                                new Card("4", "Spades", 4),
-                                new Card("5", "Spades", 5),
-                                new Card("6", "Spades", 6),
-                                new Card("7", "Spades", 7),
-                                new Card("8", "Spades", 8),
-                                new Card("9", "Spades", 9),
-                                new Card("10", "Spades", 10),
-                                new Card("Jack", "Spades", 11),
-                                new Card("Queen", "Spades", 12),
-                                new Card("King", "Spades", 13),
-                                new Card("Ace", "Diamonds", 1),
-                                new Card("2", "Diamonds", 2),
-                                new Card("3", "Diamonds", 3),
-                                new Card("4", "Diamonds", 4),
-                                new Card("5", "Diamonds", 5),
-                                new Card("6", "Diamonds", 6),
-                                new Card("7", "Diamonds", 7),
-                                new Card("8", "Diamonds", 8),
-                                new Card("9", "Diamonds", 9),
-                                new Card("10", "Diamonds", 10),
-                                new Card("Jack", "Diamonds", 11),
-                                new Card("Queen", "Diamonds", 12),
-                                new Card("King", "Diamonds", 13)
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        new Card("Ace", "Clubs", 1),
+                                        new Card("2", "Clubs", 2),
+                                        new Card("3", "Clubs", 3),
+                                        new Card("4", "Clubs", 4),
+                                        new Card("5", "Clubs", 5),
+                                        new Card("6", "Clubs", 6),
+                                        new Card("7", "Clubs", 7),
+                                        new Card("8", "Clubs", 8),
+                                        new Card("9", "Clubs", 9),
+                                        new Card("10", "Clubs", 10),
+                                        new Card("Jack", "Clubs", 11),
+                                        new Card("Queen", "Clubs", 12),
+                                        new Card("King", "Clubs", 13),
+                                        new Card("Ace", "Hearts", 1),
+                                        new Card("2", "Hearts", 2),
+                                        new Card("3", "Hearts", 3),
+                                        new Card("4", "Hearts", 4),
+                                        new Card("5", "Hearts", 5),
+                                        new Card("6", "Hearts", 6),
+                                        new Card("7", "Hearts", 7),
+                                        new Card("8", "Hearts", 8),
+                                        new Card("9", "Hearts", 9),
+                                        new Card("10", "Hearts", 10),
+                                        new Card("Jack", "Hearts", 11),
+                                        new Card("Queen", "Hearts", 12),
+                                        new Card("King", "Hearts", 13),
+                                        new Card("Ace", "Spades", 1),
+                                        new Card("2", "Spades", 2),
+                                        new Card("3", "Spades", 3),
+                                        new Card("4", "Spades", 4),
+                                        new Card("5", "Spades", 5),
+                                        new Card("6", "Spades", 6),
+                                        new Card("7", "Spades", 7),
+                                        new Card("8", "Spades", 8),
+                                        new Card("9", "Spades", 9),
+                                        new Card("10", "Spades", 10),
+                                        new Card("Jack", "Spades", 11),
+                                        new Card("Queen", "Spades", 12),
+                                        new Card("King", "Spades", 13),
+                                        new Card("Ace", "Diamonds", 1),
+                                        new Card("2", "Diamonds", 2),
+                                        new Card("3", "Diamonds", 3),
+                                        new Card("4", "Diamonds", 4),
+                                        new Card("5", "Diamonds", 5),
+                                        new Card("6", "Diamonds", 6),
+                                        new Card("7", "Diamonds", 7),
+                                        new Card("8", "Diamonds", 8),
+                                        new Card("9", "Diamonds", 9),
+                                        new Card("10", "Diamonds", 10),
+                                        new Card("Jack", "Diamonds", 11),
+                                        new Card("Queen", "Diamonds", 12),
+                                        new Card("King", "Diamonds", 13)
+                                )
                         )
                 );
 
+    }
+
+    private List<Player> createPlayers(int numPlayers) {
+        return new ArrayList() {{ add(new Player("Jeremy"));}};
     }
 
 }
