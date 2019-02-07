4bd5ff28893c597463ab1f23d20bf86f5f913a13
Jeremy Kutil <jeremykutil@gmail.com>
2019-02-06 18:09:17 -0600

    added documentation, getters/setters, and logic to the Deck class

--- src/main/java/io/kutilj/cards/Deck.java
+++ src/main/java/io/kutilj/cards/Deck.java
@@ -1,12 +1,72 @@
 package io.kutilj.cards;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A simple class that represents a Deck in a card game
+ */
 public class Deck {
 
-    List<Card> cards;
+    /**
+     * The Cards in the Deck
+     */
+    private List<Card> cards;
 
+    /**
+     * Default constructor
+     */
     public Deck() {
+        cards = new ArrayList<>();
+    }
+
+    /**
+     * Constructs a Deck with the given Cards
+     *
+     * @param cards The List of Cards given to construct the Deck
+     */
+    public Deck(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    /**
+     * Gets the current Cards in the Deck
+     *
+     * @return Returns the List of Cards in the Deck
+     */
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    /**
+     * Sets the Cards in the Deck
+     *
+     * @param cards The Cards given to the Deck
+     */
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    /**
+     * Builder method
+     * <p>
+     * Builds a Deck with the given Cards
+     *
+     * @param cards The Cards given to the Deck
+     * @return Returns a Deck
+     */
+    public Deck withCards(List<Card> cards) {
+        this.cards = cards;
+        return this;
+    }
+
+    /**
+     * Adds a Card to the Current Deck
+     *
+     * @param card The Card given to the Deck
+     */
+    public void addCard(Card card) {
+        this.cards.add(card);
     }
 
 }
